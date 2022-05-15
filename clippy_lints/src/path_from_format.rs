d9f294461352b4e2548015ba8328c43624d3f490
merelymyself <poonsters6@gmail.com>
2022-05-15 15:18:43 +0800

    added suggestion

--- clippy_lints/src/path_from_format.rs
+++ clippy_lints/src/path_from_format.rs
@@ -1,9 +1,11 @@
-use clippy_utils::diagnostics::span_lint_and_help;
-use clippy_utils::match_qpath;
+use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::ty::is_type_diagnostic_item;
+use clippy_utils::source::snippet;
 use rustc_hir::{Expr, ExprKind};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 use rustc_span::sym;
+use rustc_errors::Applicability;
 
 declare_clippy_lint! {
     /// ### What it does
@@ -31,52 +33,62 @@ declare_lint_pass!(PathFromFormat => [PATH_FROM_FORMAT]);
 impl<'tcx> LateLintPass<'tcx> for PathFromFormat {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
         if_chain! {
-            if let ExprKind::Call(func, args) = expr.kind;
-            if let ExprKind::Path(ref qpath) = func.kind;
-            if match_qpath(qpath, &["PathBuf", "from"]);
-            // if args.len() == 1;
+            if let ExprKind::Call(_, ref args) = expr.kind;
+            if let ty = cx.typeck_results().expr_ty(expr);
+            if is_type_diagnostic_item(cx, ty, sym::PathBuf);
             if let Some(macro_def_id) = args[0].span.ctxt().outer_expn_data().macro_def_id;
             if cx.tcx.get_diagnostic_name(macro_def_id) == Some(sym::format_macro);
-            // if let ExprKind::Block(block, None) = args[0].kind;
-            // if block.stmts.len() == 1;
-            // if let StmtKind::Local(local) = block.stmts[0].kind;
-            // if let Some(init) = local.init;
-            // if let ExprKind::Call(func1, args1) = init.kind;
-            // if let ExprKind::Path(ref qpath1) = func1.kind;
-            // if match_qpath(qpath1, &["$crate", "fmt", "format"]);
-            // if args1.len() == 1;
-            // if let ExprKind::Call(func2, args2) = args1[0].kind;
-            // if let ExprKind::Path(ref qpath2) = func2.kind;
-            // if match_qpath(qpath2, &["$crate", "fmt", "Arguments", "new_v1"]);
-            // if args2.len() == 2;
-            // if let ExprKind::AddrOf(BorrowKind::Ref, Mutability::Not, inner) = args2[0].kind;
-            // if let ExprKind::Array(elements) = inner.kind;
-            // if elements.len() == 2;
-            // if let ExprKind::AddrOf(BorrowKind::Ref, Mutability::Not, inner1) = args2[1].kind;
-            // if let ExprKind::Array(elements1) = inner1.kind;
-            // if elements1.len() == 1;
-            // if let ExprKind::Call(func3, args3) = elements1[0].kind;
-            // if let ExprKind::Path(ref qpath3) = func3.kind;
-            // if match_qpath(qpath3, &["$crate", "fmt", "ArgumentV1", "new_display"]);
-            // if args3.len() == 1;
-            // if let ExprKind::AddrOf(BorrowKind::Ref, Mutability::Not, inner2) = args3[0].kind;
-            // if let ExprKind::Path(ref qpath4) = inner2.kind;
-            // if match_qpath(qpath4, &["base_path"]);
-            // if let PatKind::Binding(BindingAnnotation::Unannotated, _, name, None) = local.pat.kind;
-            // if name.as_str() == "res";
-            // if let Some(trailing_expr) = block.expr;
-            // if let ExprKind::Path(ref qpath5) = trailing_expr.kind;
-            // if match_qpath(qpath5, &["res"]);
             then {
-                span_lint_and_help(
+                let full_expr = snippet(cx, expr.span, "error").to_string();
+                let split_expr: Vec<&str> = full_expr.split('!').collect();
+                let args_to_macro = split_expr[1];
+                let replaced = args_to_macro.replace('(', "").replace(')', "");
+                let unformatted: Vec<&str> = replaced.split(",").collect();
+                let mut push_targets: Vec<String> = Vec::new();
+                let mut temp_string = String::new();
+                for c in unformatted[0].chars() {
+                    if c == '/' || c == '\\' {
+                        push_targets.push(temp_string.clone());
+                        temp_string = String::new();
+                    }
+                    else if c == '}' {
+                        temp_string.push_str(&unformatted[1].replace(' ', ""));
+                    }
+                    else if c != '{' && c != '"' {
+                        temp_string.push(c);
+                    }
+                }
+                if !temp_string.is_empty() {
+                    push_targets.push(temp_string.clone());
+                    temp_string = String::new();
+                }
+                for target in push_targets {
+                    let target_processed = 
+                        if target != unformatted[1].replace(' ', "") {
+                            let mut s = String::from("\"");
+                            s.push_str(&target);
+                            s.push('"');
+                            s
+                        } 
+                        else {
+                            target
+                        };
+                    if temp_string.is_empty() {
+                        temp_string.push_str(&format!("Path::new({})", target_processed));
+                    }
+                    else {
+                        temp_string.push_str(&format!(".join({})", target_processed));
+                    }
+                }
+                span_lint_and_sugg(
                     cx,
                     PATH_FROM_FORMAT,
                     expr.span,
                     "`format!(..)` used to form `PathBuf`",
-                    None,
-                    "consider using `.join()` or `.push()` to avoid the extra allocation",
+                    "consider using `.join()` to avoid the extra allocation",
+                    temp_string,
+                    Applicability::MaybeIncorrect,
                 );
-               // report your lint here
             }
         }
     }
