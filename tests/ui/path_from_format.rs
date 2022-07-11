ef14546db63804375ad2aee2d562df27e4b0efac
merelymyself <poonsters6@gmail.com>
2022-07-11 19:05:38 +0800

    remove duplicate test

--- tests/ui/path_from_format.rs
+++ tests/ui/path_from_format.rs
@@ -10,7 +10,6 @@ fn main() {
     PathBuf::from(format!("/foo/{}/bar", base_path1));
     PathBuf::from(format!("foo/{}/bar", base_path1));
     PathBuf::from(format!("foo/foooo/{base_path1}/bar/barrr"));
-    PathBuf::from(format!("foo/foooo/{base_path1}/bar/barrr"));
     PathBuf::from(format!("foo/foooo/{base_path1}/bar/barrr/{base_path2}"));
     PathBuf::from(format!("{base_path2}/foo/{base_path1}/bar"));
     PathBuf::from(format!("foo/{base_path1}a/bar"));
